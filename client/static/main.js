f5dcb65be5aa73654817d6ae2d7b4f94a8e22bb2
egorkaBurkenya <sleepyegorka@gmail.com>
2021-06-01 19:49:40 +0300

    добавил часть обработки событий

--- client/static/main.js
+++ client/static/main.js
@@ -10,6 +10,7 @@ const pannel = document.querySelector('.pannel')
 const total = document.querySelector('.total')
 const dealerTotal = document.querySelector('.d-total')
 const yourTotal = document.querySelector('.y-total')
+const restart = document.querySelector('.restart')
 
 // * EventListeners
 
@@ -18,11 +19,11 @@ start.addEventListener('click', e => {
   hit.classList.remove('hidden')
   stay.classList.remove('hidden')
 
+  socket.emit('lemmy only one card, bruh', true)
+  socket.emit('lemmy only one card, bruh', true)
+  socket.emit('lemmy only one card, bruh', false)
+  socket.emit('lemmy only one card, bruh', false)
   socket.emit('lemmy cards, bruh')  
-  socket.emit('lemmy only one card, bruh', true)
-  socket.emit('lemmy only one card, bruh', true)
-  socket.emit('lemmy only one card, bruh', false)
-  socket.emit('lemmy only one card, bruh', false)
 })
 
 hit.addEventListener('click', e => {
@@ -33,9 +34,15 @@ stay.addEventListener('click', e => {
   stay.classList.add('hidden')
   hit.classList.add('hidden')
   restart.classList.remove('hidden')
+  // pannel.innerHTML = `<button class="restart">Restart Game</button>`
   if(dealerTotal.innerText < 16) {
     socket.emit('lemmy only one card, bruh', false)
   }
+})
+
+restart.addEventListener('click', e => {
+  socket.emit('restart')
+  window.location.reload()
 })
 
 // * functions
@@ -63,18 +70,25 @@ socket.on('card for user, bruh', card => {
 
 socket.on('card for dealer, bruh', card => {
   displayDealerCard(card.name)
-  // ! Когда чел нажимает старт, ты просишь две карты диллеру, а тут ты рисуешь ему карту 
-  // ! И тут же снова ее прочишь, если убрать эту строчку, то все работает 
   socket.emit('lemmy gamers cards count, bruh', false)
 })
 
 socket.on('user cards count, bruh', cardCount => {
+  console.log(cardCount);
   yourTotal.innerText = cardCount
 })
 
 socket.on('dealer cards count, bruh', cardCount => {
   dealerTotal.innerText = cardCount
-  // if(dealerTotal.innerText < 16) {
-  //   socket.emit('lemmy only one card, bruh', false)
-  // }
+  if (pannel.innerHTML === `<button class="restart">Restart Game</button>`) {
+    if(dealerTotal.innerText < 16) {
+      socket.emit('lemmy only one card, bruh', false)
+    } else socket.emit('open card')
+  }
+})
+
+socket.on('dealer open card, bruh', ({cardCount, card}) => {
+  // displayDealerCard()
+  dealerHand.firstElementChild.setAttribute('src', `./static/PNG/${card.name}.png`);
+  dealerTotal.innerText = cardCount
 })
